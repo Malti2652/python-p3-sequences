2ddf026590d4b32041db39ccbeacf91d4a528439
Malti2652 <eddingtonibi@gmail.com>
2024-10-13 15:43:46 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
     pass
+# def print_fibonacci(length):
+#     pass
+def print_fibonacci(n):
+    a, b = 0, 1
+    fibonacci_sequence = []
+    for _ in range(n):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+    print(fibonacci_sequence)
+# Example usage
+print_fibonacci(9)
